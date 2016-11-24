9e785a6712c38920e9039b50e04df7350f3c8194
X00122527 <X001225270@gmail.com>
2016-11-24 12:35:34 +0000

    finished lab

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import play.Mode;
 import play.api.Environment;
 import play.mvc.*;
 import play.data.*;
@@ -17,6 +18,13 @@ import models.*;
 
 public class HomeController extends Controller {
 
+    private FormFactory formFactory;
+
+    @Inject
+    public HomeController(FormFactory f){
+        this.formFactory = f;
+    }
+
     public Result index(String name) {
         return ok(index.render("Welcome to the Home page", name));
     }
@@ -30,6 +38,36 @@ public class HomeController extends Controller {
         // Get list of all categories in ascending order
         List<Product> productsList = Product.findAll();
         return ok(products.render(productsList));
+    }
+
+    public Result addProduct(){
+        Form<Product> addProductForm = formFactory.form(Product.class);
+        return ok(addProduct.render(addProductForm));
+    }
+
+    public Result addProductSubmit(){
+        Form<Product> newProductForm = formFactory.form(Product.class).bindFromRequest();
+
+        if(newProductForm.hasErrors()){
+            return badRequest(addProduct.render(newProductForm));
+        }
+
+        Product newProduct = newProductForm.get();
+
+        newProduct.save();
+
+        flash("success", "Product "+newProduct.getName() + " has been created");
+
+        return redirect(routes.HomeController.products());
+
+    }
+
+    public Result deleteProduct(Long id){
+        Product.find.ref(id).delete();
+
+        flash("success", "Product has been deleted");
+
+        return redirect(routes.HomeController.products());
     }
 
 }
